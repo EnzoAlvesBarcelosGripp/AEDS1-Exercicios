9c8cbd1b329356c2d59a37285cda939141c19184
Enzo Alves Barcelos Gripp <eabgripp@sga.pucminas.br>
2025-06-20 14:05:12 -0300

    Create exercicio 08.c #1

---
+++ Lista-02/exercicio 08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    
+    printf("escreva os dois numeros inteiros: ");
+    scanf("%d %d",&num1, &num2);
+
+    printf("O valor da soma e: %d\n", num1 + num2);
+    printf("O valor da subtracao e: %d\n", num1 - num2);
+    printf("O valor da multiplicacao e: %d\n", num1 * num2);
+
+    return 0;
+}
