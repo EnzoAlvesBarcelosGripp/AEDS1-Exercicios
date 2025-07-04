3a8ea422eb032ecc1084f37d4ca8dc4c1a31c0a6
Enzo Alves Barcelos Gripp <eabgripp@sga.pucminas.br>
2025-07-04 19:13:44 -0300

    Delete Lista-02 directory

--- Lista-02/exercicio 02.c
+++
@@ -1,5 +0,0 @@
-#include <studio.h>
-int main (){
-printf("Enzo Alves\n");
-return 0;
-}
--- Lista-02/exercicio 03.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    char letra;
-    printf("Digite um caracter:\n");
-    scanf("%c",& letra);
-    printf("Voce digitou:%c\n", letra);
-    return 0;
-}
